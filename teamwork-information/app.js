72d44736aefdca0696e7b5a762b4156a55661d3c
2644378911@qq.com <2644378911@qq.com>
2021-02-20 00:12:14 +0800

    完善uuid的判断

--- teamwork-information/app.js
+++ teamwork-information/app.js
@@ -1,36 +1,49 @@
+const ws = require("nodejs-websocket");
 
-var ws = require("nodejs-websocket");
+const groupInfo = require("./groupInfo");
 
-var arr = {};
-var server = ws
+const arr = {}; // 保留每个用户的 id，最好是以 工号+当前时间，因为每个用户在同一时间只会有一次操作
+const server = ws
   .createServer(function (scoket) {
     scoket.on("text", function (str) {
-      var data = JSON.parse(str);
+      let data = JSON.parse(str);
       console.log(str);
       if (arr[data.uuid]) {
-        for (var item in arr) {
+        for (const iterator of groupInfo) {
+          if (iterator.uuid === data.uuid.split("-")[0]) {
+            iterator.temp=data.temp
+            break;
+          }
+        }
+        for (let item in arr) {
           arr[item].sendText(
-            JSON.stringify({
-              uuid: data.uuid,
-              name: data.name,
-              date: data.date,
-              address: data.address,
-            })
+            JSON.stringify(groupInfo)
           );
         }
       } else {
-        arr[data.uuid] = scoket;
+        for (const iterator of groupInfo) {
+          if (iterator.uuid === data.uuid.split("-")[0]) {
+            scoket.sendText(JSON.stringify(groupInfo));
+            arr[data.uuid] = scoket;
+            return;
+          }
+        }
+        console.log("用户不存在");
+        scoket.sendText(
+          JSON.stringify({
+            error: "用户不存在",
+          })
+        );
+
       }
     });
     scoket.on("close", function (code, reason) {
-      console.log("关闭连接");
+      console.log("关闭连接", reason);
     });
     scoket.on("error", function (code, reason) {
-      console.log("异常关闭");
+      console.log("异常关闭", reason);
     });
   })
   .listen(3000, () => {
     console.log("running");
   });
-
-
---
+++ teamwork-information/groupInfo.js
@@ -0,0 +1,9 @@
+// 组员的工号及姓名
+module.exports = [
+    { uuid: "111", name: "赵" },
+    { uuid: "222", name: "钱" },
+    { uuid: "333", name: "孙" },
+    { uuid: "444", name: "李" },
+    { uuid: "555", name: "周" },
+    { uuid: "666", name: "吴" }
+]
